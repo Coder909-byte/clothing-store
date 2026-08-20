abe4fe5822d39473e66691762c2e14ce9265a8db
Jivesh Kumar Nazar <jivesh.nazar@gmail.com>
2026-08-20 23:39:20 +0530

    MEDUSA CONFIG

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
     },
     http: {
       storeCors: process.env.STORE_CORS ?? 'http://localhost:3000',
-      adminCors: process.env.ADMIN_CORS ?? 'http://localhost:7001',
-      authCors: process.env.AUTH_CORS ?? 'http://localhost:3000,http://localhost:7001',
+  adminCors: process.env.ADMIN_CORS ?? 'http://localhost:3000',
+  authCors: process.env.AUTH_CORS ?? 'http://localhost:3000',
       jwtSecret: process.env.JWT_SECRET ?? 'supersecret',
       cookieSecret: process.env.COOKIE_SECRET ?? 'supersecret',
     },
